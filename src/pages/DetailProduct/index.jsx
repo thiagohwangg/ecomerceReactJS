6cbddfe979a12df4da3afdfdc6bbb1a57eb74ac8
thiagohwang <cmnqpart2@gmail.com>
2025-05-16 17:32:26 +0700

    update code video 45

--- src/pages/DetailProduct/index.jsx
+++ src/pages/DetailProduct/index.jsx
@@ -1,7 +1,90 @@
-import React from 'react'
+import MyHeader from '@components/Header/Header';
+import MainLayout from '@components/Layout/Layout';
+import React from 'react';
+import styles from './styles.module.scss';
+import Button from '@components/Button/Button';
 
 export default function DetailProduct() {
-  return (
-    <div>DetailProduct</div>
-  )
+    const {
+        container,
+        navigateSection,
+        contentSection,
+        imageBox,
+        infoBox,
+        price,
+        description,
+        boxSize,
+        size,
+        titleSize,
+        functionInfo,
+        boxBtn,
+        incrementAmount,
+        orSection
+    } = styles;
+    return (
+        <div>
+            <MyHeader />
+            <div className={container}>
+                <MainLayout>
+                    <div className={navigateSection}>
+                        <div>Home {'>'} Men</div>
+                        <div style={{ cursor: 'pointer' }}>
+                            {'<'} Return to previous page
+                        </div>
+                    </div>
+                    <div className={contentSection}>
+                        <div className={imageBox}>
+                            <img
+                                src='https://xstore.8theme.com/elementor2/marseille04/wp-content/uploads/sites/2/2022/12/Image-1.1-min.jpg'
+                                alt=''
+                            />
+                            <img
+                                src='https://xstore.8theme.com/elementor2/marseille04/wp-content/uploads/sites/2/2022/12/Image-1.1-min.jpg'
+                                alt=''
+                            />
+                            <img
+                                src='https://xstore.8theme.com/elementor2/marseille04/wp-content/uploads/sites/2/2022/12/Image-1.1-min.jpg'
+                                alt=''
+                            />
+                            <img
+                                src='https://xstore.8theme.com/elementor2/marseille04/wp-content/uploads/sites/2/2022/12/Image-1.1-min.jpg'
+                                alt=''
+                            />
+                        </div>
+                        <div className={infoBox}>
+                            <h1>Title product</h1>
+                            <p className={price}>$1.23</p>
+                            <p className={description}>
+                                Amet, elit tellus, nisi odio velit ut. Euismod
+                                sit arcu, quisque arcu purus orci leo.
+                            </p>
+                            <p className={titleSize}>Size</p>
+                            <div className={boxSize}>
+                                <div className={size}>S</div>
+                                <div className={size}>M</div>
+                                <div className={size}>L</div>
+                            </div>
+
+                            <div className={functionInfo}>
+                                <div className={incrementAmount}>
+                                    <div>-</div>
+                                    <div>1</div>
+                                    <div>+</div>
+                                </div>
+                                <div className={boxBtn}>
+                                    <Button content={'Add to cart'} />
+                                </div>
+                            </div>
+
+                            <div className={orSection}>
+                                <div></div>
+                                <span>OR</span>
+                                <div></div>
+                            </div>
+                        </div>
+                    </div>
+                </MainLayout>
+            </div>
+        </div>
+    );
 }
